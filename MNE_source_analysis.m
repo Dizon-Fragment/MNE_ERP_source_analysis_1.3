558a4e4da6f3bf057f6b90589201a0f76eb9d090
Guangzhi Deng <59638771+Dizon-Fragment@users.noreply.github.com>
2022-11-26 20:39:10 +0800

    Main

--- MNE_source_analysis.m
+++ MNE_source_analysis.m
@@ -4,16 +4,9 @@
 
 %% Run
 clear; close all; clc;
+% ---------------------------- Parameter ----------------------------------
 tmp_path = strsplit(matlab.desktop.editor.getActiveFilename, filesep);
 cd(fullfile(tmp_path{1:end-1}));
 ct = 0;
 my_dialog
-% ---------------------------- Parameter ----------------------------------
-% dir_path = 'D:\helping\JiayuYao'; % set the main path, and put the data in the subfolder, like 'D:\helping\JiayuYao\data'
-% fieldtrip_path = 'D:\source\class info\FT_learning\EEG_senior\5EEGT_day1\toolboxes\fieldtrip-20180101';
-% eeglab_path = 'D:\Installation_package\eeglab14_0_0b';
-% channel_num = 61; % channel number must be identical.
-% erp_cond = {'31', '32', '41', '42'};
-% grid_resolution = 10; % mm
 % -------------------------------------------------------------------------
-
